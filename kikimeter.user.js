dc74988378225a3b3d7ab84eb8b4ef1153b79518
yLark <yoanncornec@gmail.com>
2014-10-01 18:13:18 +0200

    Ajout require jQuery
    Sous Firefox, bien que jQuery soit servi par leekwars.com, il ne semble plus exploitable par ce script.
    Dans le doute, on l'appelle.

--- kikimeter.user.js
+++ kikimeter.user.js
@@ -9,7 +9,8 @@
 // @projectPage		https://github.com/Zear06/LeekWars_Kikimeter
 // @downloadURL		https://github.com/Zear06/LeekWars_Kikimeter/raw/master/kikimeter.user.js
 // @updateURL		https://github.com/Zear06/LeekWars_Kikimeter/raw/master/kikimeter.user.js
-// @require			https://raw.githubusercontent.com/nnnick/Chart.js/master/Chart.js
+// @require		https://raw.githubusercontent.com/nnnick/Chart.js/master/Chart.js
+// @require		https://code.jquery.com/jquery-2.1.1.min.js
 // ==/UserScript==
 
 // URL DE LA PAGE PHP QUI RÉCEPTIONNE LES DONNÉES
