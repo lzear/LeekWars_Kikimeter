9f7236ba8dcbd4cf6aef373c4bdc6fb8f18af574
Foudge <olivier.fouge@free.fr>
2014-10-11 18:33:54 +0200

    Suppression du fichier *.meta.js qui servait à la MAJ du script via GM
    Le système de MAJ des scripts depuis l'extension GreaseMonkey pouvait
    passer par un fichier *.meta.js mais ça n'était pas obligatoire. Au
    final, c'est plus simple de maintenir 1 seul fichier.

--- kikimeter.user.js
+++ kikimeter.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name				LeekWars : LeeKikiMeter
-// @version				0.03a
+// @version				0.03b
 // @description			Ce script affiche un résumé des combats de leekwars
 // @match				http://leekwars.com/report/*
 // @author				Elzéar, yLark, Foudge, AlexClaw
@@ -8,7 +8,7 @@
 // @grant				GM_setValue
 // @projectPage			https://github.com/Zear06/LeekWars_Kikimeter
 // @downloadURL			https://github.com/Zear06/LeekWars_Kikimeter/raw/master/kikimeter.user.js
-// @updateURL			https://github.com/Zear06/LeekWars_Kikimeter/raw/master/kikimeter.meta.js
+// @updateURL			https://github.com/Zear06/LeekWars_Kikimeter/raw/master/kikimeter.user.js
 // @require				https://code.jquery.com/jquery-2.1.1.min.js
 // @require				https://raw.githubusercontent.com/nnnick/Chart.js/master/Chart.js
 // @require				http://kryogenix.org/code/browser/sorttable/sorttable.js
